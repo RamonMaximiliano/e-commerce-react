225a094503d929c3c3637c1006b7b72708419a55
RamonMaximiliano <ramon_max1986@hotmail.com>
2023-10-06 13:13:01 -0300

    test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,17 @@
-export const Header = () => {
+import cartImg from "../../images/carrinho-de-compras.png"
+import "./styles.css"
 
+export const Header = () => {
     return (
-        <div>
-            Hello there!
+        <div className="main-header">
+            <ul className="menu-list">
+                <li>Home</li>
+                <li>Products</li>
+                <li>Contact</li>
+            </ul>
+            <div>
+                <img src={cartImg} className="cartImg"/>
+            </div>
         </div>
     )
 }
