09ee884d77b3f0b1dfcc9c9028bd7539dd59d760
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2023-11-17 23:45:11 -0300

    cloned

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -10,33 +10,31 @@ import "./styles.css";
 export const Contact = () => {
   return (
     <div className="main-contact-div">
-      <div>
-        <img src={aiImage} className="profile-image" />
-      </div>
-
       <div className="contact-div">
         <div>
-          Contact page https://ramonmaximiliano.github.io/Portfolio2023/
-          trk=d_flagship3_profile_self_view_public_profile Hello world! Just a
-          guy learning web dev
+          <img src={aiImage} className="profile-image" />
         </div>
-        <div className="icon-list">
-          <a href="https://github.com/RamonMaximiliano" target="_blank">
-            <img src={githubreverse} />
-          </a>
-          <a
-            href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"
-            target="_blank"
-          >
-            <img src={linkedin} />
-          </a>
-          <a
-            href="https://api.whatsapp.com/send?phone=5551996927970"
-            target="_blank"
-          >
-            <img src={whats} />
-          </a>
+        <div>
+          <h3>Hello world!</h3>
+          Just a guy learning web dev
         </div>
+      </div>
+      <div className="icon-list">
+        <a href="https://github.com/RamonMaximiliano" target="_blank">
+          <img src={githubreverse} />
+        </a>
+        <a
+          href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"
+          target="_blank"
+        >
+          <img src={linkedin} />
+        </a>
+        <a
+          href="https://api.whatsapp.com/send?phone=5551996927970"
+          target="_blank"
+        >
+          <img src={whats} />
+        </a>
       </div>
     </div>
   );
