3fd8e8564d5938bf277af3203b69d20f63e53baf
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2023-12-27 23:18:51 -0300

    contacts added

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,6 +5,12 @@ import linkedin from "../../images/linkedin.png";
 import whatsreverse from "../../images/whatsapp-reverse.png";
 import aiImage from "../../images/aiImage.jpg";
 import whats from "../../images/whatsapp.png";
+import portfolioimage from "../../images/rocket.png"
+import portfolioreverse from "../../images/rocketblack.png"
+import mail from "../../images/mail.png"
+import mailblack from "../../images/mailblack.png"
+import resume from "../../images/cv.png"
+import resumeblack from "../../images/cvblack.png"
 import "./styles.css";
 import { useState, useEffect } from "react";
 
@@ -12,6 +18,9 @@ export const Contact = () => {
   const [gitHub, setGitHub] = useState(false);
   const [linkedins, setLinkedin] = useState(false);
   const [whatss, setWhats] = useState(false);
+  const [portfolio, setPortfolio] = useState(false);
+  const [resumestate, setResume] = useState(false);
+  const [mailstate, setMail] = useState(false);
 
   let setIsHovered = () => {
     setGitHub(true);
@@ -36,6 +45,25 @@ export const Contact = () => {
   let setIsNotHoveredW = () => {
     setWhats(false);
   };
+  let setIsHoveredportfolio = () => {
+    setPortfolio(true);
+  };
+  let setIsNotHoveredportfolio = () => {
+    setPortfolio(false);
+  };
+  let setIsHoveredMail = () => {
+    setMail(true);
+  };
+  let setIsNotHoveredMail = () => {
+    setMail(false);
+  };
+  let setIsHoveredResume = () => {
+    setResume(true);
+  };
+  let setIsNotHoveredResume = () => {
+    setResume(false);
+  };
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -63,6 +91,7 @@ export const Contact = () => {
         </div>
       </div>
 
+<div className="main-icon-grid">
       <div className="icon-list">
         <a
           href="https://github.com/RamonMaximiliano"
@@ -91,6 +120,37 @@ export const Contact = () => {
         >
           <img src={whatss ? whatsreverse : whats} />
         </a>
+        </div>
+        <div className="icon-list">
+        <a
+          href="https://ramonmaximiliano.github.io/Portfolio2023/"
+          target="_blank"
+          className="contact-icon"
+          onMouseEnter={setIsHoveredportfolio}
+          onMouseLeave={setIsNotHoveredportfolio}
+        >
+          <img src={portfolio ? portfolioimage : portfolioreverse} />
+        </a>
+        <a
+          href="mailto:ramon_max1986@hotmail.com"
+          target="_blank"
+          className="contact-icon"
+          onMouseEnter={setIsHoveredMail}
+          onMouseLeave={setIsNotHoveredMail}
+        >
+          <img src={mailstate ? mail : mailblack} />
+        </a>
+        <a
+          href={process.env.PUBLIC_URL + '/EnglishCV.pdf'}
+          target="_blank"
+          className="contact-icon"
+          onMouseEnter={setIsHoveredResume}
+          onMouseLeave={setIsNotHoveredResume}
+        >
+          <img src={resumestate ? resume : resumeblack} />
+        </a>
+        
+        </div>
       </div>
     </div>
   );
